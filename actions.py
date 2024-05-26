fba0790a9c7ed4be48c27629c78d28aaf860b503
ailiu75056 <ailiu75056@gmail.com>
2024-05-26 16:32:52 -0500

    added document logic to actions and added subclasses for users

--- actions.py
+++ actions.py
@@ -1,7 +1,46 @@
+from datetime import datetime
+from enum import Enum
+from users import User, Agent
+
+class ActionTypes(Enum): 
+    Collection = "Collection"
+    Testing = "Testing"
+    Transportation = "Transportation"
+    Destruction = "Destruction"
+
+class ActionStatus(Enum): 
+    Completed = "Completed"
+    In_Progress = "In Progress"
+    Disputed = "Disputed"
+
+
+class RefrigerantTypes(Enum):
+     R_32 = "R 32"
+     R_22 = "R 22"
+     R_134a = "R 134a"
+     R_404a = "R 404a"
+     R_407c = "R 407c"
+     R_410a = "R 410a"
+     Other = "Other"
+
+
+
 class Action:
-    def __init__(self, id, documents, agentUsers, createdDate, type):
+    def __init__(self, id, documents:list, agentUsers, createdDate, actionType: ActionTypes, emissionsOffset, emissionsEmitted):
+        self.netEmissions = emissionsOffset - emissionsEmitted
         self.id = id
         self.documents = documents
         self.agentUsers = agentUsers
         self.createdDate = createdDate
-        self.type = type
+        self.type = actionType
+    
+    def update_net_emissions(self):
+        self.netEmissions = self.emissions_offset - self.emissions_emitted
+    
+    def add_document(self, document):
+        if any(document.id == doc.id for doc in self.documents):
+            self.documents.append(document)
+            return "Document added to the action"
+        else:
+            return "Document already exists in the action"
+    
---
+++ users.py
@@ -0,0 +1,26 @@
+class User:
+    def __init__(self, id, firstName, lastName, middleName, dateOfBirth, userType, createdDate, company):
+        self.id = id
+        self.firstName = firstName
+        self.lastName = lastName
+        self.middleName = middleName
+        self.dateOfBirth = dateOfBirth
+        self.userType = userType
+        self.createdDate = createdDate
+        
+
+class Agent(User):
+    def __init__(self, id, firstName, lastName, middleName, dateOfBirth, userType, createdDate, company, apiId, description, employmentType):
+        super().__init__(id, firstName, lastName, middleName, dateOfBirth, userType, createdDate)
+        self.apiId = apiId
+        self.description = description
+        self.employmentType = employmentType
+        self.company = company
+
+
+class Buyer(User):
+    def __init__(self, id, firstName, lastName, middleName, dateOfBirth, userType, createdDate, buyerType, segment, paymentMethods):
+        super().__init__(id, firstName, lastName, middleName, dateOfBirth, userType, createdDate)
+        self.buyerType = buyerType
+        self.segment = segment
+        self.paymentMethods = paymentMethods
