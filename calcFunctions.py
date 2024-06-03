98de47151d98d2c7d06368eefa2730a7700167f3
ailiu75056 <ailiu75056@gmail.com>
2024-06-03 11:54:48 -0500

    return amount in $ instead of kg for consistency

--- calcFunctions.py
+++ calcFunctions.py
@@ -6,26 +6,26 @@ from datetime import datetime
 #tuple indicating the list of offsets allocated to the customer, the reamining amount, if the purchase was successfully fulled allocated
 #offsetAllocation should be a list of tuples with the offset id, customerID, and amount assigned, and datetime
 def allocateOffset(offsetList:list, purchaseAmount:float, offsetPriceKG:float, offsetAllocation:list, customerId: str) -> tuple:
-    remainingAmount = purchaseAmount/offsetPriceKG #amount to be assigned starts at the purchase amount converted to KG
+    remainingAmountKG = purchaseAmount/offsetPriceKG #amount to be assigned starts at the purchase amount converted to KG
     
     avaiableOffsets = [(x.id, x.offsetKG - sum([x for x in offsetAllocation if x[0] == x.id])) for x in offsetList]
     def efficientAllocation():
-        i = min(avaiableOffsets, key=lambda amt:abs(amt-remainingAmount)) # the offset with the amount of unallocated offset closest to the remaining amount
+        i = min(avaiableOffsets, key=lambda amt:abs(amt-remainingAmountKG)) # the offset with the amount of unallocated offset closest to the remaining amount
         return (i[0],i[1])
     
-    while remainingAmount > 0:
+    while remainingAmountKG > 0:
         offsetID, offsetKG = efficientAllocation()
         offsetAllocated= []
-        if remainingAmount > sum([x[1] for x in avaiableOffsets]): #check if available offset stock is less than the remaining amount
-            return (offsetAllocated, remainingAmount,False)
-        if offsetKG > remainingAmount:
-            offsetAllocation.append((offsetID, remainingAmount, customerId, datetime.now()))
+        if remainingAmountKG > sum([x[1] for x in avaiableOffsets]): #check if available offset stock is less than the remaining amount
+            return (offsetAllocated, remainingAmountKG*offsetPriceKG,False)
+        if offsetKG > remainingAmountKG:
+            offsetAllocation.append((offsetID, remainingAmountKG*offsetPriceKG, customerId, datetime.now()))
             offsetAllocated.append((offsetID))
-            remainingAmount = 0
-            return (offsetAllocated, remainingAmount,True) #return the offset allocated and the remaining amount of 0
+            remainingAmountKG = 0
+            return (offsetAllocated, remainingAmountKG*offsetPriceKG,True) #return the offset allocated and the remaining amount of 0
         else:
             offsetAllocation.append((offsetID, offsetKG, customerId, datetime.now()))
-            remainingAmount -= offsetKG
+            remainingAmountKG -= offsetKG
             continue #continue to the next iteration of the loop to allocate the remaining amount
 
     return None
