17d09faedc557fcc357262dc19abfff4224d94a1
ailiu75056 <ailiu75056@gmail.com>
2024-08-05 07:51:42 -0500

    removed file with token from barcode.py

--- barcode.py
+++ barcode.py
@@ -12,5 +12,5 @@ def get_barcode_from_url(image_url):
         return False
     else:
         return out[0].data.decode("utf-8")
-    
-#print(get_barcode_from_url('https://api.telegram.org/file/bot7356202275:AAGnTYB1q63URoBJ64nzXTPkUp--OB61-h0/photos/file_6.jpg'))
+
+
